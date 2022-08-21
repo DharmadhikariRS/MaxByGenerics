ebad25450ca59a0340700fe8ebb09c1d58d071e5
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-08-21 18:32:26 +0530

    [Rushikesh] MaxOf-Three-Integers

---
+++ MaxByGenerics/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MaxByGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            Console.WriteLine("Find Maximum of 3 float numbers");
+            int input;
+            int[] intArray = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter {0} number", i + 1);
+                input = int.Parse(Console.ReadLine());
+                intArray[i] = input;
+            }
+            IntMax.toPrint(intArray);
+        }
+    }
+}
